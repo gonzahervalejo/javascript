c7e0292929af236b34da46c71909088ade90af1c
gonzahervalejo <gonzalohervalejo@hotmail.com>
2021-10-25 03:16:54 -0300

    clase 2

--- entregas.js
+++ entregas.js
@@ -1,5 +1,23 @@
 // clase 1
 
 
-const edad=parseInt(prompt("¿Eres mayor de edad? Ingrese su edad"));
-alert("Mi edad es " + edad);
+// const edad=parseInt(prompt("¿Eres mayor de edad? Ingrese su edad"));
+// alert("Mi edad es " + edad);
+
+// clase 2
+
+
+const numero = parseInt(prompt("Ingrese su edad"));
+
+if (numero >= 18) {
+   alert("Bienvenido");
+
+}
+
+else if (numero < 18) {
+alert ("No puedes ingresar");
+}
+
+else {
+alert ("Ingresa tu edad con teclado numerico");  
+}
